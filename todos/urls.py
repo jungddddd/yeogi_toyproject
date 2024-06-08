7506054441e3dd69f9e5d565675bb5b18f2d03d2
HeheHeewon <qtrachel@naver.com>
2024-06-08 09:57:46 +0900

    CRUD

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,11 @@
 # todo/urls.py
 
 from django.urls import path
-from .views import TodoListAPIView, TodoDestroyAPIView, TodoListRetrieveAPIView
+from .views import TodoListAPIView, TodoDestroyAPIView, TodoListRetrieveAPIView, TodoCreateAPIView, TodoUpdateAPIView
 
 urlpatterns = [
+    path('todos/create/<int:id>/', TodoCreateAPIView.as_view(), name='todo-create'),
+    path('todos/update/<int:id>/', TodoUpdateAPIView.as_view(), name='todo-update'),
     path('todos/list', TodoListAPIView.as_view(), name='todo-list-retrieve'),  # 변경된 URL 패턴 이름
     path('todos/list/<int:id>/', TodoListRetrieveAPIView.as_view(), name='todo-detail'),
     path('todos/delete/<int:id>/', TodoDestroyAPIView.as_view(), name='todo-delete'),
