e1b51fdda7b127691ea7fac65bbcc9c2628f9815
minnoiii <shmin1202@gmail.com>
2024-06-13 16:13:49 +0900

    third

--- todos/models.py
+++ todos/models.py
@@ -9,16 +9,16 @@ from django.utils import timezone
 
 class TodoItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=True)
     content = models.TextField(blank=True, null=False)
     priority = models.IntegerField(default=1)  # 1: 낮음, 2: 중간, 3: 높음
-    image = models.ImageField(upload_to='todos/', null=False)
+    #image = models.ImageField(upload_to='todos/', null=False)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
-    original_url = models.URLField()  # 원본 자료를 볼 수 있는 URL
-    related_site_url = models.URLField()  # 관련 자료조사 사이트로 이동할 수 있는 URL
+    #original_url = models.URLField()  # 원본 자료를 볼 수 있는 URL
+    #related_site_url = models.URLField()  # 관련 자료조사 사이트로 이동할 수 있는 URL
     last_modified = models.DateTimeField(auto_now=True)  # 최근 수정일
-    due_date = models.DateTimeField()  # 마감 기한 (날짜와 시간 포함)
+    #due_date = models.DateTimeField()  # 마감 기한 (날짜와 시간 포함)
 
 
     def __str__(self):
