e23ea8f5c4b83a0c1f98f12fefe0639188be715c
jung <dnjswnddkgg@naver.com>
2024-06-14 21:41:16 +0900

    db

--- manage.py
+++ manage.py
@@ -1,16 +1,14 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-import django
-import django_rest
-
-from django.core.management import execute_from_command_line
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGOSETTINGSMODULE', 'djangorest.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest.settings')
     try:
-        from django.core.management import executefrom_command_line
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
