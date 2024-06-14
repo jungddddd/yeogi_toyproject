1a50999d29660d2f6d6c6a0e1131ee40d0ac1243
jung <dnjswnddkgg@naver.com>
2024-06-14 13:30:07 +0900

    back commit

--- todos/models.py
+++ todos/models.py
@@ -11,6 +11,7 @@ class TodoItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, null=True)
     content = models.TextField(blank=True, null=False)
+    detail = models.TextField(blank=True, null=False)
     priority = models.IntegerField(default=1)  # 1: 낮음, 2: 중간, 3: 높음
     #image = models.ImageField(upload_to='todos/', null=False)
     completed = models.BooleanField(default=False)
@@ -19,6 +20,7 @@ class TodoItem(models.Model):
     #related_site_url = models.URLField()  # 관련 자료조사 사이트로 이동할 수 있는 URL
     last_modified = models.DateTimeField(auto_now=True)  # 최근 수정일
     #due_date = models.DateTimeField()  # 마감 기한 (날짜와 시간 포함)
+
 
 
     def __str__(self):
